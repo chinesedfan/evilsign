57d01ddb321266546e57b134f71fcac52e2f5d30
chinesedfan <chinesedfan@qq.com>
2015-09-24 09:49:34 +0800

    write js draft

---
+++ index.js
@@ -0,0 +1,73 @@
+var lngInput = $('.lng-input'), latInput = $('.lat-input'),
+	tokenInput = $('.token-input');
+var signButton = $('.sign-button');
+var outputArea = $('.output-section');
+
+var MAGIC_WORD = '';
+
+$(function() {
+	loadCookies();
+	$('.J_sign-button').on('click', sendEvilSign);
+});
+
+function loadCookies() {
+	var result = {};
+    document.cookie.split(";").forEach(function(p) {
+        var c = p.trim().split("=");
+        var name = c[0];
+        var value = c[1];
+        result[name] = value;
+    });
+
+    lngInput.val(result['evillng'] || '');
+    latInput.val(result['evillat'] || '');
+    tokenInput.val(result['eviltoken'] || '');
+}
+
+function encode(str){
+	var arr = [];
+	for (var i = 0; i < str.length; i++) {
+		arr.push('x' + str.charCodeAt(i).toString(16));
+	}
+	return arr.join('');
+}
+function decode(str){
+	var arr = str.split('x'), res = [];
+	for (var i = 0; i < arr.length; i++) {
+		res.push(String.fromCharCode(parseInt(arr[i], 16)));
+	}
+	return res.join('');
+}
+
+function getData() {
+	var data = {
+		longitude: parseFloat(lngInput.val()),
+		latitude: parseFloat(latInput.val()),
+		uuid: tokenInput.val()
+	};
+
+	document.cookie = 'evillng=' + data.longitude;
+	document.cookie = 'evillat=' + data.latitude;
+	document.cookie = 'eviltoken=' + data.uuid;
+
+	data.longitude += Math.random() * 0.002 - 0.001;
+	data.latitude += Math.random() * 0.002 - 0.001;
+
+	return data;
+}
+function sendEvilSign() {
+	$.ajax({
+		url: decode(MAGIC_WORD),
+		type: 'POST',
+		data: getData(),
+		success: function(data, status, xhr) {
+			// body...
+		},
+		error: function(xhr, errorType, error) {
+			// body...
+		},
+		complete: function(xhr, status) {
+			// body...
+		}
+	});
+}
